70a21b5cd686ea518da845e3e9177f5b0c382e09
Carloselx <c.maselx@gmail.com>
2024-09-18 18:31:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // Cargar fechas no disponibles del LocalStorage, si existen
-const unavailableDates = JSON.parse(localStorage.getItem('unavailableDates')) || [
-    '2024-09-20', // Ejemplo de fecha no disponible
-    '2024-10-15', // Otra fecha no disponible
-    // Añadir más fechas según sea necesario
-];
+const unavailableDates = JSON.parse(localStorage.getItem('unavailableDates')) || {};
 
 // Cargar reservas del LocalStorage, si existen
 const reservations = JSON.parse(localStorage.getItem('reservations')) || [];
@@ -28,17 +24,15 @@ function calculatePrice(selectedDate, timeSlot) {
 // Función para manejar el cambio de fecha
 function onDateChange() {
     const selectedDate = document.getElementById('date').value;
-    const isHoliday = unavailableDates.includes(selectedDate);
-    
-    if (isHoliday) {
-        alert("La fecha seleccionada no está disponible.");
+    const timeSlot = document.getElementById('timeSlot').value;
+
+    if (unavailableDates[selectedDate] && unavailableDates[selectedDate].includes(timeSlot)) {
+        alert("La franja horaria seleccionada no está disponible.");
         document.getElementById('price').value = "";
         return;
     }
 
-    const timeSlot = document.getElementById('timeSlot').value;
     const price = calculatePrice(selectedDate, timeSlot);
-    
     document.getElementById('price').value = price ? `${price} euros` : "";
 }
 
@@ -63,14 +57,17 @@ document.getElementById('rentalForm').addEventListener('submit', function(event)
     const phone = document.getElementById('phone').value;
     const price = document.getElementById('price').value;
 
-    // Verifica si la fecha está disponible
-    if (unavailableDates.includes(selectedDate)) {
-        alert('La fecha seleccionada ya está marcada como no disponible.');
+    // Verifica si la fecha y franja horaria están disponibles
+    if (unavailableDates[selectedDate] && unavailableDates[selectedDate].includes(timeSlot)) {
+        alert('La franja horaria seleccionada ya está marcada como no disponible.');
         return;
     }
 
-    // Añadir la fecha a las fechas no disponibles y guardarla en LocalStorage
-    unavailableDates.push(selectedDate);
+    // Añadir la franja horaria a las fechas no disponibles
+    if (!unavailableDates[selectedDate]) {
+        unavailableDates[selectedDate] = [];
+    }
+    unavailableDates[selectedDate].push(timeSlot);
     localStorage.setItem('unavailableDates', JSON.stringify(unavailableDates));
 
     // Guardar la reserva en LocalStorage
