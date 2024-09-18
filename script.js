5ad52ee868570b378260314e03075f2dcffe94f9
Carloselx <c.maselx@gmail.com>
2024-09-18 17:23:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -75,6 +75,15 @@ document.getElementById('rentalForm').addEventListener('submit', function(event)
     unavailableDates.push(selectedDate);
     localStorage.setItem('unavailableDates', JSON.stringify(unavailableDates));
 
+    // Imprimir los datos en la consola
+    console.log({
+        date: selectedDate,
+        timeSlot: timeSlot,
+        email: email,
+        phone: phone,
+        price: price
+    });
+
     // Enviar correo electrónico a ti mismo con EmailJS
     if (typeof emailjs !== 'undefined') {
         emailjs.send('service_lqiluoz', 'template_v0l0n99', {
@@ -87,10 +96,11 @@ document.getElementById('rentalForm').addEventListener('submit', function(event)
         .then(function(response) {
             alert('Reserva confirmada. Se ha enviado un correo electrónico de confirmación.');
         }, function(error) {
-            console.error('Error:', error);  // Captura el error
+            console.error('Error:', error);  // Imprime el error en la consola para detalles más específicos
             alert('Error al enviar el correo electrónico. Inténtalo de nuevo.');
         });
     } else {
         alert('Error: No se pudo enviar el correo. Verifica la configuración de EmailJS.');
     }
 });
+
